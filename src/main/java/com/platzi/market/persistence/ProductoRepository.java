19c21a9bfdce369615b00e0daa490204b7ee2a53
caobandoc <obandocarlosandres@gmail.com>
2022-05-07 11:45:28 -0500

    @Repository

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,10 +2,13 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+//@Component sirve para que Spring sepa que es un componente
+@Repository // pero se usa @Repository para que sepa que especificamente es repositorio
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -17,15 +20,19 @@ public class ProductoRepository {
         return (List<Producto>) productoCrudRepository.findByIdCategoria(IdCategoria);
     }
 
-    public List<Producto> getByCategoriaQuery(int IdCategoria) {
-        return (List<Producto>) productoCrudRepository.getByCategoria(IdCategoria);
-    }
-
-    public List<Producto> getByCategoriaOrder(int IdCategoria) {
-        return (List<Producto>) productoCrudRepository.findByIdCategoriaOrderByNombreAsc(IdCategoria);
-    }
-
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
