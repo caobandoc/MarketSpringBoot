4a1c00820d6e5919de751242f0b28b20d5f37f99
caobandoc <obandocarlosandres@gmail.com>
2022-05-07 00:24:13 -0500

    Entity para base de datos

---
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -0,0 +1,43 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private String idCategoria;
+    private String descripcion;
+    private String estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+
+    public String getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(String idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
